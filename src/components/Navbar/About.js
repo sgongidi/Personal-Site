7602a42267b47be1379e19ae27e5b2e4e595afc8
Sai Gongidi <saigongidi@gmail.com>
2020-07-05 01:57:27 -0400

    initial commit

---
+++ src/components/Navbar/About.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+export default class About extends Component {
+    render() {
+        return (
+            <li className='nav-item'>
+                <a class='nav-link' href='/'>About</a>
+            </li>
+        );
+    }
+}
